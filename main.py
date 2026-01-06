4cdf871e45007b5661521cf182c3a9de7f4a74ef
rishijhunjhunu <mytemporaryemailforpersonaluse@gmail.com>
2026-01-06 20:20:05 +0530

    Update main.py
    Signed-off-by: rishijhunjhunu <mytemporaryemailforpersonaluse@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
 from pyrogram import Client
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 app = Client(
-   "bot",
-   api_id=os.getenv("
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+app.run()
